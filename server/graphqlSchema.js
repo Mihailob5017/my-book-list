332c41750f19943fe9f97cacf8cfdf032eace5b3
Mihailo Boskovic <mihailob5017@gmail.com>
2020-02-03 17:02:58 +0100

    adding realtions

--- server/graphqlSchema.js
+++ server/graphqlSchema.js
@@ -15,6 +15,29 @@ const axios = require('axios').default;
 //
 //
 //
+//  Dummy Data
+var books = [
+  { name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1' },
+  { name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2' },
+  { name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2' },
+  { name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3' },
+  { name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3' },
+  { name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3' }
+];
+
+var authors = [
+  { name: 'Patrick Rothfuss', age: 44, id: '1' },
+  { name: 'Brandon Sanderson', age: 42, id: '2' },
+  { name: 'Terry Pratchett', age: 66, id: '3' }
+];
+//
+//
+//
+//
+//
+//
+//
+//
 //  Getting th Book
 const BookType = new GraphQLObjectType({
   name: 'Book',
@@ -22,10 +45,13 @@ const BookType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    authorId: { type: GraphQLString },
     author: {
       type: AuthorType,
-      args: { id: { type: GraphQLString } },
-      resolve(parent, args) {}
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return authors.find(author => author.id === parent.authorId);
+      }
     }
   })
 });
@@ -45,7 +71,15 @@ const AuthorType = new GraphQLObjectType({
     id: { type: GraphQLID },
     age: { type: GraphQLInt },
     name: { type: GraphQLString },
-    books: { type: new GraphQLList(BookType) }
+    books: {
+      type: new GraphQLList(BookType),
+      args: {
+        name: { type: GraphQLString }
+      },
+      resolve(parent, args) {
+        return books.filter(book => book.authorId === parent.id);
+      }
+    }
   })
 });
 //
@@ -63,27 +97,27 @@ const RootQuery = new GraphQLObjectType({
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        return axios.get('').then(res => res.data);
+        return books;
       }
     },
     book: {
       type: BookType,
-      args: { id: { type: GraphQLID }, name: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
-        return axios.get('').then(res => res.data);
+        return books.find(book => book.id === args.id);
       }
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
-        return axios.get('').then(res => res.data);
+        return authors;
       }
     },
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID }, name: { type: GraphQLString } },
       resolve(parent, args) {
-        return axios.get('').then(res => res.data);
+        return authors.find(author => author.id === args.id);
       }
     }
   }
