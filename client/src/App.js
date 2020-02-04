a9b6d174bee668cbbdd4071992cb5afa3402df0b
Mihailo Boskovic <mihailob5017@gmail.com>
2020-02-04 21:28:21 +0100

    set up apollo

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
-
+import './style.css';
+//  Apollo Setup
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+//
+//  Components
+import BookList from './components/BookList';
+//
+//  Apollo Connect
+const Client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+});
+//
+//  Main React App
 const App = () => {
-  return <div>hello from react!</div>;
+  return (
+    <ApolloProvider client={Client}>
+      <div className="main">
+        <h1>Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
+  );
 };
 
 export default App;
